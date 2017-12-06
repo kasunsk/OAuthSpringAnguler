591b7a39f2257b53b386fad2d6ca57f6d749a543
Kasun Kariyawasam <kasunsk@gmail.com>
2017-12-06 18:31:54 +0530

    first commit

---
+++ src/main/java/com/auth/server/AuthServerDemoApplication.java
@@ -0,0 +1,12 @@
+package com.auth.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServerDemoApplication.class, args);
+	}
+}
